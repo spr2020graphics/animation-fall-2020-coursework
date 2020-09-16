4c5b06aca83beaddf056880f46cc9353cece665f
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-09-15 19:01:12 -0500

    Adding parser frame

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationIO.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationIO.c
@@ -1,5 +1,6 @@
 #include "A3_DEMO/_animation/a3_KeyframeAnimationIO.h"
 
+
 a3i32 a3clipParse(a3_DemoState* state, a3byte const* data)
 {
 	return 0;
@@ -13,6 +14,30 @@ a3i32 a3keyframeParse(a3_DemoState* state, a3byte const* data)
 a3i32 a3animationParseFile(a3_DemoState* state, a3byte const* data)
 {
 	int clipPoolSize = 0;
+	int keyframePoolSize = 0;
+
+	char* token;
+
+	// Just a delimiter function, using it to pull out whole lines, since the material file is formatted in a certain way
+	token = strtok((char*)data, "\n");
+
+	while (token != NULL)
+	{
+		switch (token[0])
+		{
+		case '#':
+			break;
+		case '@':
+			clipPoolSize++;
+			break;
+		case '$':
+			keyframePoolSize++;
+			break;
+		default:
+			break;
+		}
+	}
+
 	state->clipCount = clipPoolSize;
 	a3clipPoolCreate(state->clipPool, state->clipCount);
 	return 0;
