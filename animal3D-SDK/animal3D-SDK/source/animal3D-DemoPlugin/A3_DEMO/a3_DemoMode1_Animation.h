af6ec2644e0481c3303f49a9a8bf6d0a915879f8
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-09-23 13:54:09 -0500

    Various necessary functions in the forgotten inline, also added the animation update calls

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation.h
@@ -33,7 +33,7 @@
 //-----------------------------------------------------------------------------
 
 #include "_a3_demo_utilities/a3_DemoSceneObject.h"
-
+#include "_animation/a3_Kinematics.h"
 #include "_animation/a3_HierarchyStateBlend.h"
 
 
