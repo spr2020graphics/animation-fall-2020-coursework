74cd26963efdd32f8ad7fb0441d836f1c5f4dbd1
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-10-03 13:53:13 -0400

    channel count done

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
@@ -176,6 +176,7 @@ a3i32 a3hierarchyPoseGroupLoadBVH(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 		char* token = strtok((char*)contentsCopy, "\n");
 
 		a3ui32 nodeCount = 0;
+		a3ui32 channelCount = 0;
 		while (token != NULL)
 		{
 			while (token[0] == ' ' || token[0] == '\t')
@@ -186,6 +187,10 @@ a3i32 a3hierarchyPoseGroupLoadBVH(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 			if (strncmp(token, "JOINT", 5) == 0 || strncmp(token, "ROOT", 4) == 0 || strncmp(token, "End", 3) == 0)
 			{
 				nodeCount++;
+			}
+			if (strncmp(token, "CHANNELS", 8) == 0)
+			{
+				channelCount += atoi(token + 9);
 			}
 
 			token = strtok(NULL, "\n");
