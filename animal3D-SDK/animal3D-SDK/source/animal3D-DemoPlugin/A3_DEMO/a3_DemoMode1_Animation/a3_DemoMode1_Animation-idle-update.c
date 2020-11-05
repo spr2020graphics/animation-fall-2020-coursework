d41e9248db7722f4e4cf3581ee4e6d0850ae29ff
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-11-05 15:50:38 -0500

    interp1 works

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
@@ -269,15 +269,15 @@ void a3animation_update(a3_DemoState* demoState, a3_DemoMode1_Animation* demoMod
 			break;
 		case animation_input_interpolate1:
 		{
-			a3real2 targetPos, targetRot;
-			targetPos[0] = demoMode->pos.x + (a3real)demoMode->axis_l[0];
-			targetPos[1] = demoMode->pos.y + (a3real)demoMode->axis_l[1];
+			a3real2 targetPos;
+			a3real targetRot;
+			targetPos[0] = (a3real)demoMode->axis_l[0];
+			targetPos[1] = (a3real)demoMode->axis_l[1];
 
-			targetRot[0] = demoMode->rot + ((a3real)demoMode->axis_r[0]);
-			targetRot[1] = 0.0f;
+			targetRot = ((a3real)demoMode->axis_r[0]);
 
 			a3real2Lerp(demoMode->pos.v, demoMode->pos.v, targetPos, 0.4f);
-			a3real2Lerp(&demoMode->rot, &demoMode->rot, targetRot, 0.2f);
+			demoMode->rot = a3lerp(demoMode->rot, targetRot, 0.4f);
 		}
 			break;
 		case animation_input_interpolate2:
