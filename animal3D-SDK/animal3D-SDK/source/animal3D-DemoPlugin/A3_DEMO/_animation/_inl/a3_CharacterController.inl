db9dc31c3b5c8da4563c41960148a8546df4fc21
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-11-15 14:58:04 -0600

    Adding basic character controller struct

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_inl/a3_CharacterController.inl
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_inl/a3_CharacterController.inl
@@ -6,7 +6,23 @@
 //-----------------------------------------------------------------------------
 
 
+inline a3ui32 a3characterControllerInit(a3_CharacterController* controller_out, a3_ClipController* controller, a3f32 jump, a3f32 walkThreshold, a3f32 runThreshold)
+{
+	controller_out->animController = controller;
+	controller_out->jumpTrigger = jump;
+	controller_out->maxWalkThreshold = walkThreshold;
+	controller_out->maxRunThreshold = runThreshold;
 
+	return 1;
+}
+
+inline a3ui32 a3characterControllerApplyInput(a3_DemoSceneObject* object, a3vec2* position, a3real rotation)
+{
+	object->position.x = +(position->x);
+	object->position.y = +(position->y);
+
+	object->euler.z = -a3trigValid_sind(rotation);
+}
 
 //-----------------------------------------------------------------------------
 
--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/a3_CharacterController.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/a3_CharacterController.h
@@ -4,19 +4,39 @@
 #define __ANIMAL3D_CHARACTER_CONTROLLER_H
 
 
-#include "animal3D/a3/a3types_integer.h"
+#include "a3_KeyframeAnimationController.h"
 
 
 #ifdef __cplusplus
 extern "C"
 {
 #else	// !__cplusplus
+typedef struct a3_CharacterController		a3_CharacterController;
 #endif	// __cplusplus
 
 
 //-----------------------------------------------------------------------------
 
+struct a3_CharacterController
+{
+	// reference to the animation controller for this character
+	a3_ClipController* animController;
 
+	// similar to the former "branchTrigger" from lab 4, this is the input container for the jump input value, and will also trigger a transition into the jump animation
+	a3f32 jumpTrigger;
+
+	// this is an input threshold to trigger the walk cycle (I think we'll use tri-lerp and some fancy normalization logic to get the weights)
+	a3f32 maxWalkThreshold;
+
+	// this is an input threshold to trigger the run cycle (greater than the walkThreshold)
+	a3f32 maxRunThreshold;
+};
+
+// init a character controller
+a3ui32 a3characterControllerInit(a3_CharacterController* controller_out, a3_ClipController* controller, a3f32 jumpTrigger, a3f32 walkThreshold, a3f32 runThreshold);
+
+// apply position and rotation input to an object
+a3ui32 a3characterControllerApplyInput(a3_DemoSceneObject* object, a3vec2* position, a3real rotation);
 
 //-----------------------------------------------------------------------------
 
